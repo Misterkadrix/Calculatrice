4e2764f46d5327a9093f5b2c5fb42829050f7717
kad-sa@outlook.fr <kad-sa@outlook.fr>
2020-10-02 09:44:11 +0200

    j'ai oublié de rajouter le 0

--- public/js/Fonctionnalité.js
+++ public/js/Fonctionnalité.js
@@ -121,7 +121,7 @@ r4Btn3.addEventListener("click",()=>{
 });
 let body = document.body;
 body.addEventListener("keydown",(e) => {
-    if (e.key== "+" ||e.key== "-" ||e.key== "*" ||e.key== "/" ||e.key== "1" ||e.key== "2" ||e.key== "3" ||e.key== "4" ||e.key== "5" ||e.key== "6" || e.key== "7" || e.key== "8" ||e.key== "9") {
+    if (e.key== "+" ||e.key== "-" ||e.key== "*" ||e.key== "/" ||e.key== "1" ||e.key== "2" ||e.key== "3" ||e.key== "4" ||e.key== "5" ||e.key== "6" || e.key== "7" || e.key== "8" ||e.key== "9"||e.key=="0") {
         input1.value += e.key;
     } else if (e.key =="="){
             stock1 = input1.value;
